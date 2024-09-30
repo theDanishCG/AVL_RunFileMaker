535a8c7ed07ba0c4bebeca630b0b5a2434f7cec4
DanishChainGobbler <jackson.s.dane@gmail.com>
2024-09-29 20:38:12 -0500

    Fixed standard atmospheric interpolation function and added comments

--- AVL_RunFileMaker.py
+++ AVL_RunFileMaker.py
@@ -11,15 +11,21 @@ Created: 09/26/2024
 
 """
 
+# import packages
+
 import os
 import numpy as np
 from pathlib import Path
+
+# define paths and files
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 inFile = "input.csv"
 templateFile = Path(str(dir_path) + "/data/template.run")
 atmosFile = templateFile = Path(str(dir_path) + "/data/stdatmos.csv")
 outFile = "cases.run"
+
+# initialize lists
 
 varNames = []
 alt = []
@@ -33,8 +39,33 @@ pList = []
 rhoList = []
 aList = []
 muList = []
+caseatmos = []
 
+# Create output messages
+
+inpBegin = "Opening input file and reading variables"
 inpComplete = "Input cases read successfully"
+tempRead = "Reading template run file"
+
+def getAtmos(alt):
+
+    """
+    This function interpolates standard atmospheric conditions
+    for the given altitude
+    """
+
+    retCond = []
+
+    retCond.append(np.interp(alt,altArray,tempArray))
+    retCond.append(np.interp(alt,altArray,pArray))
+    retCond.append(np.interp(alt,altArray,rhoArray))
+    retCond.append(np.interp(alt,altArray,aArray))
+    retCond.append(np.interp(alt,altArray,muArray))
+
+    return retCond # Returns list of floats containing standard atmospheric
+                   # conditions at provided altitude
+
+# Reads standard atmospheric data for interpolation
 
 with open(atmosFile, 'r') as aF:
     for i, line in enumerate(aF):
@@ -47,24 +78,29 @@ with open(atmosFile, 'r') as aF:
             aList.append(templine[4])
             muList.append(templine[5])
 
-altArray = np.array(altList)
-tempArray = np.array(tempList)
-pArray = np.array(pList)
-rhoArray = np.array(rhoList)
-aArray = np.array(aList)
-muArray = np.array(muList)
+# Converts atmospheric data lists into arrays and deletes lists
+
+altArray = np.asarray(altList, dtype = np.float32)
+tempArray = np.asarray(tempList, dtype = np.float32)
+pArray = np.asarray(pList, dtype = np.float32)
+rhoArray = np.asarray(rhoList, dtype = np.float32)
+aArray = np.asarray(aList, dtype = np.float32)
+muArray = np.asarray(muList, dtype = np.float32)
 
 del altList, tempList, pList, rhoList, aList, muList
 
+# Reads input file and creates lists of variables for all cases
+
+print(inpBegin)
 with open(inFile, 'r') as iF:
-    print("Opening input file and reading variables")
     for i, line in enumerate(iF):
         if i == 0:
             templine = line.split()
             for j, k in enumerate(templine):
                 varNames.append(templine[j])
         else:
-            print("Reading inputs for case " + str(i) + " of " + str(len(varNames)))
+            print("Reading inputs for case " + str(i) + " of " +
+                str(len(varNames)))
             templine = line.split(',')
             alt.append(templine[0])
             M.append(templine[1])
@@ -73,30 +109,17 @@ with open(inFile, 'r') as iF:
 
 print(inpComplete)
 
-print("Formatting output run file")
+# Reads template run file to rebuild with case data
+
+print(tempRead)
 
 with open(templateFile, 'r') as tF:
     for line in tF:
         tempLines.append(line)
+        caseAtmos = getAtmos(alt[0])
 print('Template format loaded')
 #with open(outFile, 'w') as oF:
     #for i in len(alt):
         #for i, line in enumerate(tempLines):
         # Modify each
 
-def getAtmos(alt):
-
-"""
-This function interpolates standard atmospheric conditions
-for the given altitude
-"""
-
-retCond = []
-
-retCond.append(interp(alt,altArray,tempArray))
-retCond.append(interp(alt,altArray,pArray))
-retCond.append(interp(alt,altArray,rhoArray))
-retCond.append(interp(alt,altArray,aArray))
-retCond.append(interp(alt,altArray,muArray))
-
-return retCond
